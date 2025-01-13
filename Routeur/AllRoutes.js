ba69514dea79c7ec00a0ca47003358a2fb5a9265
Lasdepic <clementjordan34@gmail.com>
2025-01-13 20:42:25 +0100

    réservation avec plage horaire

--- Routeur/AllRoutes.js
+++ Routeur/AllRoutes.js
@@ -6,10 +6,11 @@ export const allRoutes = [
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
     new Route("/carte", "La carte", "/pages/carte.html"),
     new Route("/allResa", "Vos réservations", "/pages/reservation/allResa.html"),
+    new Route("/reserver", "Réserver", "/pages/reservation/reserver.html"),
     new Route("/signin", "connexion", "/pages/auth/signin.html"),
     new Route("/signup", "inscription", "/pages/auth/signup.html"),
     new Route("/account", "mon compte", "/pages/auth/account.html"),
-    new Route("/editPassword", "changer le mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
 ];
 
 
