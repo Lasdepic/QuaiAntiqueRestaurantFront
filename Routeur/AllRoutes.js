731995bef9b35d0a1c5bdc582980a33a0f8b03c1
Lasdepic <clementjordan34@gmail.com>
2025-01-15 18:37:54 +0100

    code en js

--- Routeur/AllRoutes.js
+++ Routeur/AllRoutes.js
@@ -8,7 +8,7 @@ export const allRoutes = [
     new Route("/allResa", "Vos réservations", "/pages/reservation/allResa.html"),
     new Route("/reserver", "Réserver", "/pages/reservation/reserver.html"),
     new Route("/signin", "connexion", "/pages/auth/signin.html"),
-    new Route("/signup", "inscription", "/pages/auth/signup.html"),
+    new Route("/signup", "inscription", "/pages/auth/signup.html", "/Js/auth/signup.js"),
     new Route("/account", "mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
 ];
